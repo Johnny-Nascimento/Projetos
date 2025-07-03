e7fcbfc27394216f386c3dca1c44ce22992f6e29
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-07-03 15:46:58 -0300

    - Adicionado controller para post de URL e criado classes para trabalhar com os links, adicionado lógica simples inicial para geração de link encurtado.

---
+++ Encurtador-De-Links/Controllers/EncurtadorController.cs
@@ -0,0 +1,26 @@
+﻿using Encurtador_De_Links.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Encurtador_De_Links.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EncurtadorController : ControllerBase
+{
+    private readonly List<LinkEncurtado> Links = new List<LinkEncurtado>();
+
+    [HttpPost(Name = "PostEncurtador")]
+    public void AdicionaLink([FromBody] Link link)
+    {
+        Guid id = Guid.NewGuid();
+
+        string linkCurto = $"https://localhost:7245/Encurtador/{id}";
+
+        LinkEncurtado linkEncurtado = new LinkEncurtado(id, DateTime.Now, link.Original, linkCurto, inativo:false);
+
+        Links.Add(linkEncurtado);
+
+        foreach (var item in Links)
+            Console.WriteLine(item.ToString());
+    }
+}
---
+++ Encurtador-De-Links/Models/Link.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Encurtador_De_Links.Models;
+
+public class Link
+{
+    [Required(ErrorMessage = "Link original é obrigatório")]
+    [Url]
+    public string Original{ get; set; } = string.Empty; // Talvez remover classe e trabalhar com URI ou URL ou alguma classe especifica para links
+
+    public override string ToString()
+    {
+        return String.Format("Original:{0}", Original);
+    }
+}
+
+public class LinkEncurtado
+{
+    public Guid Id { get; }
+    public DateTime DataHoraCriacao { get; }
+    public string Original { get; } = string.Empty; // Talvez remover classe e trabalhar com URI ou URL ou alguma classe especifica para links
+    public string Encurtado { get; } = string.Empty; // Talvez remover classe e trabalhar com URI ou URL ou alguma classe especifica para links
+    public bool Inativo { get; }
+
+    public LinkEncurtado(Guid id, DateTime dataHoraCriacao, string original, string encurtado, bool inativo)
+    {
+        Id = id;
+        DataHoraCriacao = dataHoraCriacao;
+        Original = original;
+        Encurtado = encurtado;
+        Inativo = inativo;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Id:{0}, DataHoraCriacao:{1}, Original:{2}, Encurtado:{3}, Inativo:{4}", Id, DataHoraCriacao, Original, Encurtado, Inativo);
+    }
+}
