7e124f9e44bcf3ed680c789a3d9aa42277201d7c
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-07-15 17:09:44 -0300

    - Adicionado notações required e key para classe link

--- Encurtador-De-Links/Models/Link.cs
+++ Encurtador-De-Links/Models/Link.cs
@@ -4,10 +4,21 @@ namespace Encurtador_De_Links.Models;
 
 public class Link
 {
+    [Key]
+    [Required]
     public Guid Id { get; private set; }
+
+    [Required]
     public DateTime DataHoraCriacao { get; private set; }
+
+    [Required]
+    [Url]
     public string Original { get; private set; } = string.Empty; // Talvez remover classe e trabalhar com URI ou URL ou alguma classe especifica para links
+
+    [Required]
     public string Encurtado { get; private set; } = string.Empty; // Talvez remover classe e trabalhar com URI ou URL ou alguma classe especifica para links
+
+    [Required]
     public bool Inativo { get; private set; }
 
     public Link(Guid id, DateTime dataHoraCriacao, string original, string encurtado, bool inativo = false)
